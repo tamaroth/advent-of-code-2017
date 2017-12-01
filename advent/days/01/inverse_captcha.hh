4d12ef5344dfe3b4f43d1046d452dc0c0a8e8ff2
tamaroth <tamaroth@tamaroth.eu>
2017-12-01 14:34:14 +0100

    Add Day 1 solutions.
    There are solutions to both parts of first day puzzles.

---
+++ advent/days/01/inverse_captcha.hh
@@ -0,0 +1,28 @@
+/// @file
+///
+/// Day 1: Inverse Captcha
+///
+
+#include <iostream>
+#include <string>
+
+#include "advent/utils/base.hh"
+
+namespace advent {
+
+class Day01: public Day {
+public:
+	Day01() = default;
+	~Day01() override = default;
+
+	virtual void solve_part_one() override;
+	virtual void solve_part_two() override;
+
+	virtual bool test_part_one() override;
+	virtual bool test_part_two() override;
+
+private:
+	int solve_for_input(const std::string& input, int distance = 1);
+};
+
+}
